b88ead82023cdb2efe7eefe73f5f5cdb6195fb97
Erik Carlsson <info@code301.com>
2014-12-19 16:18:51 +0100

    "Init."

---
+++ lib/Map/Metro/Oslo.pm
@@ -0,0 +1,28 @@
+package Map::Metro::Oslo;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Map::Metro::Oslo - Short intro
+
+=head1 SYNOPSIS
+
+  use Map::Metro::Oslo;
+
+=head1 DESCRIPTION
+
+Map::Metro::Oslo is ...
+
+=head1 SEE ALSO
+
+=cut
